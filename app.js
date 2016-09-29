4194294c026ca862a6afde07587d9042ce472c1b
junwoo ahn <junwoo ahn>
2016-09-29 17:20:47 +0900

    안준우

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
-  res.send('Hello World');
+app.get('/', function(req, res) {
+    res.send('Hello World');
 });
 
-app.get('/junwoo',function(req,res){
-  res.send('Hello Junwoo');
+app.get('/junwoo', function(req, res) {
+    res.send('Hello Junwoo22');
 });
 
-app.listen(3000,function(){
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 })
