a3a5e6845e0a14ee747e322ee4608f6916c41fb9
junwoo ahn <junwoo ahn>
2016-09-30 14:16:58 +0900

    dd

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var app = express();
+
 app.use(express.static('public'));
 app.get('/', function(req, res) {
     res.send('Hello World');
 });
-
+app.get('/dynamic',function(req,res){
+    var lis = '';
+    for(var i=0; i<5; i++){
+      lis = lis +'<li>coding</li>';
+    }
+    var time = new Date();
+    var output = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title></title>
+      </head>
+      <body>
+          Hello Dynamic!
+          <ul>
+          ${lis}
+          </ul>
+          ${time}
+      </body>
+    </html>`;
+    res.send(output);
+});
 app.get('/route',function(req,res){
     res.send('Hello 보노보노, <img src="/K-001.jpg"></img>');
 });
