934d4972ef9e00b887db06068f71070ccc89e031
junwoo ahn <junwoo ahn>
2016-09-30 11:19:04 +0900

    준우 테스트

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
-
+app.use(express.static('public'));
 app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
+app.get('/route',function(req,res){
+    res.send('Hello 보노보노, <img src="/K-001.jpg"></img>');
+});
 app.get('/junwoo', function(req, res) {
     res.send('Hello Junwoo22---');
 });
