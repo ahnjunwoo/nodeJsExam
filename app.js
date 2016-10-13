026b5f97d39aaed12691099406c2ee5357f31153
DUCKKING-PC\duckk <cutesboy2@gmail.com>
2016-10-13 09:47:58 +0900

    테스트

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 app.locals.pretty=true;
 app.set('view engine','jade');
 app.set('views','./views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.get('/form', function(req, res) {
+    res.render('form');
+});
+app.get('/form_receiver', function(req, res) {
+    var title = req.query.title;
+    var desc = req.query.desc;
+    res.send(title+','+desc);
+});
+app.post('/form_receiver', function(req, res) {
+  var title = req.body.title;
+  var desc = req.body.desc;
+  res.send(title+','+desc);
+});
+app.get('/topic/:id', function(req,res){
+  var topics=[
+    'Javascript is....',
+    'Node is....',
+    'Express si...'
+  ];
+  var output = `
+    <a href="/topic/0">Javascript</a><br/>
+    <a href="/topic/1">Node js</a><br/>
+    <a href="/topic/2">Express</a><br/>
+    ${topics[req.params.id]}
+  `;
+  res.send(output);
+});
+app.get('/topic/:mode', function(req,res){
+  res.send(req.params.id+','+req.params.mode);
+});
 app.get('/template', function(req, res) {
     res.render('temp',{time:Date(),title:'Jade'});
 });
